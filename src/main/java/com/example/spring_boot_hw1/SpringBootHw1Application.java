08c47edf6f5e51a3171f478f348986951c89f4fb
helloworld <sergeychernonosov21229@gmail.com>
2023-02-28 20:37:57 +0300

    Initial commit

---
+++ src/main/java/com/example/spring_boot_hw1/SpringBootHw1Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_hw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHw1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHw1Application.class, args);
+    }
+
+}
